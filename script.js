368ebe45f8edbb0e59ff9ea176b639c11888252d
dhruba371 <dhrubabiswaslogin371@gmail.com>
2024-02-20 22:29:04 +0600

    update the Seats left site

--- script.js
+++ script.js
@@ -1,86 +1,69 @@
 let totalSeatboocked = [ ];
 let seatCount = 0 ;
 let totalPrice = 0;
-console.log(totalPrice);
-console.log(seatCount);
-
+let anemst = 20 ;
 
 function a1(){
-   takeASeat('a1');
-   totalSeatboocked.push("a1");
-   seatCount = totalSeatboocked.length ;
-    totalPrice = 550 * seatCount ;
-   
-  const updatePrice = document.getElementById('totalPrice').innerText = totalPrice;
-  const  seat = document.getElementById('totalSeat').innerText = seatCount ;  
+   takeASeat('a1');  
 }
 function a2(){
-   takeASeat('a2');
-   totalSeatboocked.push("a2");
-   seatCount = totalSeatboocked.length ;
-    totalPrice = 550 * seatCount ;
-
-  const updatePrice = document.getElementById('totalPrice').innerText = totalPrice;
-  const  seat = document.getElementById('totalSeat').innerText = seatCount ;  
+   takeASeat('a2'); 
 }
 function b1(){
-   takeASeat('b1');
-   totalSeatboocked.push("b1");
-   seatCount = totalSeatboocked.length ;
-    totalPrice = 550 * seatCount ;
-
-  const updatePrice = document.getElementById('totalPrice').innerText = totalPrice;
-  const  seat = document.getElementById('totalSeat').innerText = seatCount ;  
+   takeASeat('b1');  
 }
 function b2(){
    takeASeat('b2');
-   totalSeatboocked.push("b2");
-   seatCount = totalSeatboocked.length ;
-    totalPrice = 550 * seatCount ;
-
-  const updatePrice = document.getElementById('totalPrice').innerText = totalPrice;
-  const  seat = document.getElementById('totalSeat').innerText = seatCount ;  
 }
 function c1(){
-   takeASeat('c1');
-   totalSeatboocked.push("c1");
-   seatCount = totalSeatboocked.length ;
-    totalPrice = 550 * seatCount ;
-
-  const updatePrice = document.getElementById('totalPrice').innerText = totalPrice;
-  const  seat = document.getElementById('totalSeat').innerText = seatCount ;  
+   takeASeat('c1'); 
 }
 function c2(){
    takeASeat('c2');
-   totalSeatboocked.push("c2");
-   seatCount = totalSeatboocked.length ;
-    totalPrice = 550 * seatCount ;
-
-  const updatePrice = document.getElementById('totalPrice').innerText = totalPrice;
-  const  seat = document.getElementById('totalSeat').innerText = seatCount ;  
 }
 function d1(){
    takeASeat('d1');
-   totalSeatboocked.push("d1");
-   seatCount = totalSeatboocked.length ;
-    totalPrice = 550 * seatCount ;
-
-  const updatePrice = document.getElementById('totalPrice').innerText = totalPrice;
-  const  seat = document.getElementById('totalSeat').innerText = seatCount ;  
 }
 function d2(){
    takeASeat('d2');
-   totalSeatboocked.push("d2");
-   seatCount = totalSeatboocked.length ;
-    totalPrice = 550 * seatCount ;
-
-  const updatePrice = document.getElementById('totalPrice').innerText = totalPrice;
-  const  seat = document.getElementById('totalSeat').innerText = seatCount ;  
 }
-
+function e1(){
+   takeASeat('e1');
+}
+function e2(){
+   takeASeat('e2');
+}
+function a3(){
+   takeASeat('a3');
+}
+function a4(){
+   takeASeat('a4');
+}
+function b3(){
+   takeASeat('b3');
+}
+function b4(){
+   takeASeat('b4');
+}
+function c3(){
+   takeASeat('c3');
+}
+function c4(){
+   takeASeat('c4');
+}
+function d3(){
+   takeASeat('d3');
+}
+function d4(){
+   takeASeat('d4');
+}
+function e3(){
+   takeASeat('e3');
+}
+function e4(){
+   takeASeat('e4');
+}
 
 seatCount = totalSeatboocked.length ;
 totalPrice = 550 * seatCount ;
-console.log('total countable of seat' , seatCount);
-console.log('total price of' , totalPrice);
 
--- utility.js
+++ utility.js
@@ -5,4 +5,17 @@ function takeASeat(giveTheSeatId){
   classesOfSeat.add('bg-[#1DD100]' , 'text-white');
   document.getElementById(giveTheSeatId).classList.add('block');
   fixedSeat.disabled = true;
+
+  anemst = anemst -1 ;
+  totalPrice = 550 * seatCount ;
+  
+  totalSeatboocked.push(giveTheSeatId);
+  seatCount = totalSeatboocked.length ;
+   totalPrice = 550 * seatCount ;
+    
+    const leftSeat = document.getElementById('available-seat').innerText = anemst ;
+   document.getElementById('grand-total').innerText = totalPrice ;
+  const updatePrice = document.getElementById('totalPrice').innerText = totalPrice;
+  const  seat = document.getElementById('totalSeat').innerText = seatCount ;
+  
 }
